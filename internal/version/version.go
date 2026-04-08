4e93d9edfb5436eea348a682a065a16698377799
Shawn Anderson <sanderson@eye-catcher.com>
2026-04-08 19:53:24 -0400

    chore: bump version to 0.0.3

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,7 +2,7 @@ package version
 
 // Set via ldflags at build time. Defaults here serve local `go build` without flags.
 var (
-	Version = "0.0.2"
+	Version = "0.0.3"
 	Commit  = "unknown"
 	Date    = "unknown"
 )
